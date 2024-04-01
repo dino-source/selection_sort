de8902ee7b58e11a72b92eefb2a226232e1b2524
Dino Source <dinosource.cpp@gmail.com>
2024-03-31 22:14:05 -0700

    Modify test case 1

--- test/selection_sort_test.cpp
+++ test/selection_sort_test.cpp
@@ -5,6 +5,6 @@
 TEST(TestTopic, selection_sort_test1) {
     std::vector<int> nums {23, 56, 11, 45, 99, 10, 57, 78};
     Solution::selection_sort(nums);
-
-    EXPECT_TRUE(std::is_sorted(nums.begin(), nums.end()));
+    bool actual_result {std::is_sorted(nums.begin(), nums.end())};
+    EXPECT_TRUE(actual_result);
 }
